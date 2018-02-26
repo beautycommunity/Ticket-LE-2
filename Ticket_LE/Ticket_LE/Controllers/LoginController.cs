0b0c73459f48460780e9787c83ef2cf2fe7feb90
สุปรีชา นิรมลพานิช <film_ten@hotmail.com>
2018-02-26 09:41:14 +0700

    ..
    ..

---
+++ Ticket_LE/Ticket_LE/Controllers/LoginController.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ticket_LE.DATA;
+using Ticket_LE.Models;
+
+namespace Ticket_LE.Controllers
+{
+    public class LoginController : Controller
+    {
+        string userOnline;
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            userOnline = GetCookie();
+            if (userOnline != string.Empty)
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(USER_LOGIN model, string returnUrl)
+        {
+            int LoginCase = 0;
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (LoginBase(model))
+            {
+                LoginCase = 1;
+
+                if (checkedepartment(model))
+                {
+                    LoginCase = 2;
+                }
+
+            }
+
+
+            switch (LoginCase)
+            {
+                case 1: // Complete Redirect
+                    Session["User"] = model.STCODE;
+                    TempData["User"] = model.STCODE;
+                    SetCookie(model.STCODE);
+                    return RedirectToLocal(returnUrl);
+                case 2: //Lock out
+                    return RedirectToAction("updatedid", model);
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
+        }
+
+        private bool checkedepartment(USER_LOGIN model)
+        {
+
+            Data_UserDataContext Context = new Data_UserDataContext();
+
+            var queryX = Context.MAS_USERs.Where(x => x.STCODE == model.STCODE && x.PASSWORD == model.PASSWORD).FirstOrDefault();
+
+            if (queryX.D_ID == 0)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
+        public bool LoginBase(USER_LOGIN model)
+        {
+            bool OK = false;
+
+            Data_UserDataContext Context = new Data_UserDataContext();
+
+            var queryX = Context.MAS_USERs.Where(x => x.STCODE == model.STCODE && x.PASSWORD == model.PASSWORD).Count();
+
+            if (queryX > 0)
+            {
+
+                OK = true;
+            }
+
+            return OK;
+        }
+
+        public ActionResult updatedid(USER_LOGIN model)
+        {
+            Dp_List ul = new Dp_List();
+            Data_UserDataContext db = new Data_UserDataContext();
+
+            ul.STCODE = model.STCODE;
+            ul.JT_LIST = new SelectList(db.MAS_DEPs, "DP_ID", "DEPARTMENT");
+
+            return View(ul);
+        }
+
+        [HttpPost]
+        public ActionResult updatedid(Dp_List model)
+        {
+
+            //if (!chkSesionUser()) { return RedirectToAction("Login", "Login", new { returnUrl = "~/TicketApprove/Index" }); }
+            using (Data_UserDataContext db = new Data_UserDataContext())
+            {
+
+                var q = (from pp in db.MAS_USERs
+                         where pp.STCODE == model.STCODE
+                         select pp).FirstOrDefault();
+
+                q.D_ID = (int)model.JT_ID;
+
+                db.SubmitChanges();
+
+            }
+
+            return RedirectToAction("Index", "Ticket");
+        }
+
+
+
+        // GET:
+
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Ticket");
+        }
+
+        public bool chkSesionUser()
+        {
+            bool chk = true;
+
+            userOnline = GetCookie();
+
+            if (userOnline == string.Empty)
+            {
+                try
+                {
+                    userOnline = Session["User"].ToString();
+                    if (userOnline.Length < 1)
+                    {
+                        chk = false;
+                    }
+                }
+                catch
+                {
+                    chk = false;
+                }
+            }
+
+            using (Data_UserDataContext Context = new Data_UserDataContext())
+            {
+                try
+                {
+                    var sql = (from xx in Context.MAS_USERs
+                               where xx.STCODE == userOnline
+                               select xx).FirstOrDefault();
+
+                    if (sql != null)
+                    {
+                        Session["SharedName"] = "สวัสดี " + sql.FNAME;
+                    }
+                    else
+                    {
+                        Session["SharedName"] = "เข้าสู่ระบบ";
+                    }
+                }
+                catch
+                {
+                    Session["SharedName"] = "เข้าสู่ระบบ";
+                }
+            }
+
+            return chk;
+        }
+
+        private void SetCookie(string User)
+        {
+            try
+            {
+                Request.Cookies["bbStcode"].Value = User;
+            }
+            catch
+            {
+                HttpCookie BeautyCookies = new HttpCookie("bbStcode");
+                BeautyCookies.Value = User;
+                BeautyCookies.Expires = DateTime.Now.AddDays(1);
+
+                Response.Cookies.Add(BeautyCookies);
+            }
+            //Request.Cookies["bbStcode"].Value = User;   
+        }
+
+
+        private string GetCookie()
+        {
+            string cookievalue = string.Empty;
+
+            if (Request.Cookies["bbStcode"] != null)
+            {
+                cookievalue = Request.Cookies["bbStcode"].Value.ToString();
+            }
+
+            return cookievalue;
+        }
+
+
+        private void RemoveCookie()
+        {
+            if (Request.Cookies["bbStcode"] != null)
+            {
+                Response.Cookies["bbStcode"].Expires = DateTime.Now.AddDays(-1);
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            try
+            {
+                Session["User"] = null;
+                Session["DP"] = null;
+                Session["SharedName"] = null;
+                RemoveCookie();
+            }
+            catch
+            {
+                Session["User"] = "";
+                Session["DP"] = "";
+                Session["SharedName"] = "";
+                RemoveCookie();
+                return RedirectToAction("Login", "Login");
+            }
+
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ Ticket_LE/Ticket_LE/Models/TicketModels.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ticket_LE.Models
+{
+    public class TicketModels
+    {
+        public List<CheckBox> Detail { get; set; }
+        public Ticket TicketSub { get; set; }
+        public List<Ticket> TicketDetail { get; set; }
+        public DetailTciket Add { get; set; }
+        public List<CheckBox> GetCheck { get; set; }
+    }
+
+    public class CheckBox
+    {
+        public int row { get; set; }
+
+        [Display(Name = "เอกสารที่ขอ")]
+        public int ID { get; set; }
+
+        [Display(Name = "วัตถุประสงค์ในการใช้เอกสาร")]
+        public string NAME { get; set; }
+
+        [Display(Name = "เอกสารที่ขอ")]
+        public string Doc { get; set; }
+
+        [Display(Name = "ประเภท")]
+        public string Type { get; set; }
+
+        public bool Checked { get; set; }
+    }
+
+    public class DetailTciket
+    {
+        [Display(Name = "วัตถุประสงค์ในการใช้เอกสาร")]
+        public string Detail { get; set; }
+
+        public bool Checked { get; set; }
+    }
+
+    public class Ticket
+    {
+        public int? ID { get; set; }
+
+        [Display(Name = "เลขที่")]
+        public string TICKETNO { get; set; }
+
+        [Display(Name = "ผู้แจ้ง")]
+        public string CRE_NICKNAME { get; set; }
+
+        [Display(Name = "สถานะ")]
+        public string SSNAME { get; set; }
+
+        [Display(Name = "ไอดีสถานะ")]
+        public int? SSID { get; set; }
+
+        [Display(Name = "แผนก")]
+        public string DEP { get; set; }
+
+        [Display(Name = "วันที่")]
+        public string CREATEDATE { get; set; }
+
+        [Display(Name = "เวลา")]
+        public string CREATETIME { get; set; }
+
+        [Display(Name = "วัตถุประสงค์ในการใช้เอกสาร")]
+        public string DETAIL { get; set; }
+
+        public bool Checked { get; set; }
+
+        [Display(Name = "ผู้ขอเอกสาร")]
+        public string NAME_OPEN { get; set; }
+        public string DATE_OPEN { get; set; }
+
+        [Display(Name = "ผู้อนุมัติขอเอกสาร")]
+        public string NAME_HDEP { get; set; }
+        public string DATE_HDEP { get; set; }
+
+        [Display(Name = "ผู้ส่งเอกสาร")]
+        public string NAME_RECEIVE { get; set; }
+        public string DATE_RECEIVE { get; set; }
+
+        [Display(Name = "ผู้อนุมัติส่งเอกสาร")]
+        public string NAME_CLOSE { get; set; }
+        public string DATE_CLOSE { get; set; }
+
+        public int? APP_ID { get; set; }
+    }
+
+
+}
